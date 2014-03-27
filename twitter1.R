24cb4dc58003c09ec62b4bcb132fc10e6001941f
Danny Silitonga <dannysilitonga@yahoo.com>
2014-03-26 21:55:34 -0400

    Here are my commits

---
+++ twitter1.R
@@ -0,0 +1,21 @@
+library("ROAuth")
+library("twitteR")
+library("wordcloud")
+library("tm")
+
+download.file(url="http://curl.haxx.se/ca/cacert.pem", destfile="cacert.pem")
+# Set SSL certs globally
+options(RCurlOptions = list(cainfo = system.file("CurlSSL", "cacert.pem", package = "RCurl")))
+
+
+cred <- OAuthFactory$new(consumerKey='',
+                         consumerSecret='',
+                         requestURL='https://api.twitter.com/oauth/request_token',
+                         accessURL='https://api.twitter.com/oauth/access_token',
+                         authURL='https://api.twitter.com/oauth/authorize')
+
+#necessary step for Windows
+cred$handshake(cainfo="cacert.pem")
+#save for later use for Windows
+save(cred, file="twitter authentication.Rdata")
+registerTwitterOAuth(cred)
---
+++ twitter2.R
@@ -0,0 +1,36 @@
+library(twitteR)
+library(tm)
+library(wordcloud)
+library(RColorBrewer)
+
+library(RCurl)
+
+
+mach_tweets = searchTwitter("gay marriage", n=499, lang="en")
+
+mach_text = sapply(mach_tweets, function(x) x$getText())
+
+# create a corpus
+mach_corpus = Corpus(VectorSource(mach_text))
+
+# create document term matrix applying some transformations
+tdm = TermDocumentMatrix(mach_corpus,
+                         control = list(removePunctuation = TRUE,
+                                        stopwords = c("machine", "learning", stopwords("english")),
+                                        removeNumbers = TRUE, tolower = TRUE))
+# define tdm as matrix
+m = as.matrix(tdm)
+# get word counts in decreasing order
+word_freqs = sort(rowSums(m), decreasing=TRUE)
+# create a data frame with words and their frequencies
+dm = data.frame(word=names(word_freqs), freq=word_freqs)
+
+# plot wordcloud
+wordcloud(dm$word, dm$freq, random.order=FALSE, colors=brewer.pal(8, "Dark2"))
+
+# save the image in png format
+png("franco.png", width=12, height=8, units="in", res=720)
+wordcloud(dm$word, dm$freq, random.order=FALSE, colors=brewer.pal(8, "Dark2"))
+dev.off()
+
+
